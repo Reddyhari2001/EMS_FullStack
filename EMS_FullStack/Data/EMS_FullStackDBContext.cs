00164bc63809bc580ce5b49cfd92577504f545ab
hariprasafdreddy <hariprasad1999@gmail.com>
2024-02-12 21:04:38 +0530

    Add project files.

---
+++ EMS_FullStack/Data/EMS_FullStackDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EMS_FullStack.Models;
+
+namespace EMS_FullStack.Data
+{
+    public class EMS_FullStackDBContext : DbContext
+    {
+        public EMS_FullStackDBContext (DbContextOptions<EMS_FullStackDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EMS_FullStack.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
